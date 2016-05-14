28ec46970915d4de9e921b473042548a8c2ecea9
Matías Argañaraz <arganarazmatias@gmail.com>
2016-05-14 20:49:46 -0300

    Fix, again

--- app/services/auth.service.js
+++ app/services/auth.service.js
@@ -1,6 +1,6 @@
 angular.module('AuthModule')
 .constant('URL_API','../api/v1/product')
-.factory('authService',['$http', '$cookies', '$rootScope', 'URL_API',function($http, $cookies, $rootScope, URL_API) {
+.factory('authService',['$http', '$cookies', '$rootScope', '$window', 'URL_API',function($http, $cookies, $rootScope, $window, URL_API) {
     var _loggedIn, _token;
     _loggedIn = false;
     _token = $cookies.get("trelloToken");
@@ -14,24 +14,30 @@ angular.module('AuthModule')
              return _loggedIn;
          },
          isCookieSetted : function() {
-             return $cookies.get("trelloToken") !== undefined;
+             _token = (_token) ? (_token) : $cookies.get("trelloToken");
+             return _token !== undefined;
          },
          logIn : function() {
              _token = (_token) ? _token : $cookies.get("trelloToken");
-             var interactive = _token == undefined;
+             var interactive = _token === undefined;
+             console.log('token ', _token);
+             console.log('trello token ', Trello.token());
              console.log('interactive ', interactive);
-             Trello.authorize({
-                 name: "Scrum Attack",
-                 type: "popup",
-                 interactive: interactive,
-                 expiration: "never",
-                 persist: true,
-                 success: function() {
-                     $rootScope.isAuthorized = _loggedIn = true;
-                     return onAuthorizeSuccessful();
-                 },
-                 scope: { write: true, read: true },
-             });
+             console.log('rootScope ', $rootScope.isAuthorized);
+             var params = {
+                     name: "Scrum Attack",
+                     type: "popup",
+                     interactive: interactive,
+                     expiration: "never",
+                     persist: true,
+                     success: function() {
+                         $rootScope.isAuthorized = _loggedIn = true;
+                         return onAuthorizeSuccessful();
+                     },
+                     scope: { write: true, read: true },
+                 };
+                 console.log('json ', params);
+             Trello.authorize(params);
          },
          logOut : function() {
              Trello.deauthorize();
@@ -39,6 +45,7 @@ angular.module('AuthModule')
              _token = undefined;
              $cookies.put("trelloToken", undefined);
              $rootScope.isAuthorized = _loggedIn = false;
+             $window.location.reload();
          }
      }
 
