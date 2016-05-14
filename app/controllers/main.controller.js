2d6e819cd7db598519ad24aa6fb9bcd0808dbb53
Matías Argañaraz <arganarazmatias@gmail.com>
2016-05-14 05:55:40 -0300

    Log in cookie

--- app/controllers/main.controller.js
+++ app/controllers/main.controller.js
@@ -1,25 +1,35 @@
 angular.module('scrumattack')
-  .controller('MainController', ['$rootScope', '$scope', 'boardService', function($rootScope, $scope, boardService) {
+  .controller('MainController', ['$rootScope', '$scope', '$cookies', 'boardService', function($rootScope, $scope, $cookies, boardService) {
     $scope.logIn = function() {
-      Trello.authorize({
-        name: "Scrum Attack",
-        type: "popup",
-        interactive: true,
-        expiration: "1hour",
-        persist: true,
-        success: function () { onAuthorizeSuccessful($rootScope); },
-        scope: { write: false, read: true },
-      });
+        Trello.authorize({
+            name: "Scrum Attack",
+            type: "popup",
+            interactive: true,
+            expiration: "1hour",
+            persist: true,
+            success: function () { onAuthorizeSuccessful($rootScope, $cookies); },
+            scope: { write: false, read: true },
+        });
 
-      function onAuthorizeSuccessful($rootScope) {
-        $rootScope.token = Trello.token();
-        getUser($rootScope);
-      }
+        function onAuthorizeSuccessful($rootScope, $cookies) {
+            $rootScope.token = Trello.token();
+            $cookies.put("loggedIn", "true");
+            getUser($rootScope);
+        }
+    };
+
+    $scope.logged = function() {
+        return $cookies.get("loggedIn") !== undefined;
+    };
+
+    $scope.logOut = function() {
+        $cookies.put("loggedIn", undefined);
     };
 
     function getUser($rootScope) {
       //boardService.getUser().success(alert("qwe"));
       var success = function(successMsg) {
+
         $rootScope.user = successMsg;
       };
 
@@ -28,5 +38,5 @@ angular.module('scrumattack')
       };
       Trello.get('member/me', success, error);
     };
-    
+
 }]);
